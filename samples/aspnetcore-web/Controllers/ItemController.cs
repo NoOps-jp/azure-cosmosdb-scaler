3d1c92c6cd38d8d4cab65013add45803ccb58809
Kazuyuki Miyake <miyake@zenarc.jp>
2018-07-23 14:57:12 +0900

    Adding dotnetcore webapp

---
+++ samples/aspnetcore-web/Controllers/ItemController.cs
@@ -0,0 +1,109 @@
+﻿namespace todo.Controllers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+
+    public class ItemController : Controller
+    {
+        private readonly IDocumentDBRepository<todo.Models.Item> Respository;
+        public ItemController(IDocumentDBRepository<todo.Models.Item> Respository)
+        {
+            this.Respository = Respository;
+        }
+
+        [ActionName("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var items = await Respository.GetItemsAsync(d => !d.Completed);
+            return View(items);
+        }
+        
+
+#pragma warning disable 1998
+        [ActionName("Create")]
+        public async Task<IActionResult> CreateAsync()
+        {
+            return View();
+        }
+#pragma warning restore 1998
+
+        [HttpPost]
+        [ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAsync([Bind("Id,Name,Description,Completed")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                await Respository.CreateItemAsync(item);
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        [HttpPost]
+        [ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAsync([Bind("Id,Name,Description,Completed")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                await Respository.UpdateItemAsync(item.Id, item);
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        [ActionName("Edit")]
+        public async Task<ActionResult> EditAsync(string id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            Item item = await Respository.GetItemAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        [ActionName("Delete")]
+        public async Task<ActionResult> DeleteAsync(string id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            Item item = await Respository.GetItemAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmedAsync([Bind("Id")] string id)
+        {
+            await Respository.DeleteItemAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        [ActionName("Details")]
+        public async Task<ActionResult> DetailsAsync(string id)
+        {
+            Item item = await Respository.GetItemAsync(id);
+            return View(item);
+        }
+    }
+}
---
+++ samples/aspnetcore-web/DocumentDBRepository.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace todo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Documents;
+    using Microsoft.Azure.Documents.Client;
+    using Microsoft.Azure.Documents.Linq;
+
+    
+    public class DocumentDBRepository<T> : IDocumentDBRepository<T> where T : class
+    {
+        private DocumentClient client;
+        private DocumentDBOptions _options;
+
+        public DocumentDBRepository(IOptions<DocumentDBOptions> options)
+        {
+            _options = options.Value;
+            this.client = new DocumentClient(new Uri(_options.AccountEndpoint), _options.AccountKeys);
+            CreateDatabaseIfNotExistsAsync().Wait();
+            CreateCollectionIfNotExistsAsync().Wait();
+        }
+
+        public async Task<T> GetItemAsync(string id)
+        {
+            try
+            {
+                Document document = await client.ReadDocumentAsync(UriFactory.CreateDocumentUri(_options.Database, _options.Collection, id));
+                return (T)(dynamic)document;
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    return null;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        public async Task<IEnumerable<T>> GetItemsAsync(Expression<Func<T, bool>> predicate)
+        {
+            IDocumentQuery<T> query = client.CreateDocumentQuery<T>(
+                UriFactory.CreateDocumentCollectionUri(_options.Database, _options.Collection),
+                new FeedOptions { MaxItemCount = -1 })
+                .Where(predicate)
+                .AsDocumentQuery();
+
+            List<T> results = new List<T>();
+            while (query.HasMoreResults)
+            {
+                results.AddRange(await query.ExecuteNextAsync<T>());
+            }
+
+            return results;
+        }
+
+        public async Task<Document> CreateItemAsync(T item)
+        {
+            return await client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(_options.Database, _options.Collection), item);
+        }
+
+        public async Task<Document> UpdateItemAsync(string id, T item)
+        {
+            return await client.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(_options.Database, _options.Collection, id), item);
+        }
+
+        public async Task DeleteItemAsync(string id)
+        {
+            await client.DeleteDocumentAsync(UriFactory.CreateDocumentUri(_options.Database, _options.Collection, id));
+        }
+
+        private async Task CreateDatabaseIfNotExistsAsync()
+        {
+            try
+            {
+                await client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(_options.Database));
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    await client.CreateDatabaseAsync(new Database { Id = _options.Database });
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private async Task CreateCollectionIfNotExistsAsync()
+        {
+            try
+            {
+                await client.ReadDocumentCollectionAsync(UriFactory.CreateDocumentCollectionUri(_options.Database, _options.Collection));
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    await client.CreateDocumentCollectionAsync(
+                        UriFactory.CreateDatabaseUri(_options.Database),
+                        new DocumentCollection { Id = _options.Collection },
+                        new RequestOptions { OfferThroughput = 1000 });
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+
+    public class DocumentDBOptions
+    {
+        public string AccountEndpoint { get; set; }
+        public string AccountKeys { get; set; }
+        public string Database { get; set; }
+        public string Collection { get; set; }
+    }
+}
