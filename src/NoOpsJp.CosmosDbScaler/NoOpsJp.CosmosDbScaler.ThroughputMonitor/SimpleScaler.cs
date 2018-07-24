bea6a8d9a46439d8aad3dffc78d083eac002729f
Kazuyuki Miyake <miyake@zenarc.jp>
2018-07-24 21:01:41 +0900

    Adding TODO comment

--- src/NoOpsJp.CosmosDbScaler/NoOpsJp.CosmosDbScaler.ThroughputMonitor/SimpleScaler.cs
+++ src/NoOpsJp.CosmosDbScaler/NoOpsJp.CosmosDbScaler.ThroughputMonitor/SimpleScaler.cs
@@ -27,6 +27,8 @@ namespace NoOpsJp.CosmosDbScaler.ThroughputMonitor
             var offersFeed = await _client.ReadOffersFeedAsync();
             var currentOffer = offersFeed.Single(o => o.ResourceLink == collection.SelfLink);
 
+            // TODO: Insert judgment on whether to actually execute Scale
+
             // change throuput
             var replaced = await _client.ReplaceOfferAsync(new OfferV2(currentOffer, scaleRequest.TargetThroughput));
             var newOffer = (OfferV2)replaced;
