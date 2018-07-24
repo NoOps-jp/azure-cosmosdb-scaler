f4d7cac99674ceea52eca986e7b7e8e61ca366b3
Kazuyuki Miyake <miyake@zenarc.jp>
2018-07-24 17:40:14 +0900

    implimented SimpleScaler

--- src/NoOpsJp.CosmosDbScaler/NoOpsJp.CosmosDbScaler.ThroughputMonitor/SimpleScaler.cs
+++ src/NoOpsJp.CosmosDbScaler/NoOpsJp.CosmosDbScaler.ThroughputMonitor/SimpleScaler.cs
@@ -1,18 +1,43 @@
-﻿using System;
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace NoOpsJp.CosmosDbScaler.ThroughputMonitor
 {
     public class SimpleScaler
     {
-        public SimpleScaler(string collectionId)
-        {
+        private readonly DocumentClient _client;
+        private readonly string _databaseId;
+        private readonly string _collectionId;
 
+        public SimpleScaler(DocumentClient client, string databaseId, string collectionId)
+        {
+            _client = client;
+            _databaseId = databaseId;
+            _collectionId = collectionId;
         }
 
-        public ScaleResponse AdjustThroughput(ScaleRequest scaleRequest)
+        public async Task<ScaleResponse> AdjustThroughputAsync(ScaleRequest scaleRequest)
         {
-            throw new NotImplementedException();
-            // TODO
+            DocumentCollection collection =
+                await _client.ReadDocumentCollectionAsync(UriFactory.CreateDocumentCollectionUri(_databaseId, _collectionId));
+            
+            // get current throuput
+            var offersFeed = _client.ReadOffersFeedAsync().Result;
+            var currentOffer = offersFeed.Single(o => o.ResourceLink == collection.SelfLink);
+
+            // change throuput
+            var replaced = await _client.ReplaceOfferAsync(new OfferV2(currentOffer, scaleRequest.TargetThroughput));
+            var newOffer = (OfferV2)replaced;
+
+            // return response
+            var response = new ScaleResponse
+            {
+                IsAccepted = true,
+                AdjustedThroughput = newOffer.Content.OfferThroughput
+            };
+            return response;
         }
     }
 }
