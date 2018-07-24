27baf9a74448e2e7530329b4a5227b0394799c91
shibayan <me@shibayan.jp>
2018-07-24 16:22:37 +0900

    Adding sample app implement

--- samples/aspnetcore-web/IDocumentDBRepository.cs
+++ samples/aspnetcore-web/IDocumentDBRepository.cs
@@ -2,16 +2,15 @@
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
-using Microsoft.Azure.Documents;
 
 namespace todo
 {
     public interface IDocumentDBRepository<T> where T : class
     {
-        Task<Document> CreateItemAsync(T item);
+        Task CreateItemAsync(T item);
         Task DeleteItemAsync(string id);
         Task<T> GetItemAsync(string id);
         Task<IEnumerable<T>> GetItemsAsync(Expression<Func<T, bool>> predicate);
-        Task<Document> UpdateItemAsync(string id, T item);
+        Task UpdateItemAsync(string id, T item);
     }
 }
