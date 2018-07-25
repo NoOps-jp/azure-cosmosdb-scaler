68d2ae9b38c986bd932da1cce519ab52256fae15
Nicolas <t-niriga@microsoft.com>
2018-07-25 17:54:49 +0900

    Updating TooManyRequests support

--- src/NoOpsJp.CosmosDbScaler/NoOpsJp.CosmosDbScaler/Strategies/TooManyRequestsStrategy.cs
+++ src/NoOpsJp.CosmosDbScaler/NoOpsJp.CosmosDbScaler/Strategies/TooManyRequestsStrategy.cs
@@ -10,30 +10,30 @@ namespace NoOpsJp.CosmosDbScaler.Strategies
     class TooManyRequestsStrategy : IScaleStrategy<double>
     {
         private List<long> _tooManyRequestsHistory = new List<long>();
+        // TODO make configurable
+        private int _monitorDuration = -5;
+        private int _tooManyRequestsThreshold = 4;
+        private int _scaleFactor = 2;
 
         public SimpleScaler Scaler { get ; set ; }
 
-        public void AddRequestCharge(double requestCharge)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task AddTooManyRequests()
-        {
+        public void AddRequestCharge(double context)
+        { 
             var eventTime = DateTime.Now;
             long tooManyRequestsEvent = eventTime.Ticks;
 
             _tooManyRequestsHistory.Add(tooManyRequestsEvent);
 
             _tooManyRequestsHistory = _tooManyRequestsHistory
-                .Where(record => record >= eventTime.AddSeconds(-5).Ticks)
+                .Where(record => record >= eventTime.AddSeconds(_monitorDuration).Ticks)
                 .ToList();
 
-            if (_tooManyRequestsHistory.Count >= 4)
+            if (_tooManyRequestsHistory.Count >= _tooManyRequestsThreshold)
             {
-                var currentThroughput = await Scaler.GetCurrentThroughputAsync();
-                var doubleRequest = new ScaleRequest("ToDoList", "Items", currentThroughput * 2 );
-                var result = Scaler.AdjustThroughputAsync(doubleRequest).Result;
+                var currentThroughput = Scaler.GetCurrentThroughputAsync().Result;
+                // TODO make dynamic 
+                var scaleRequest = new ScaleRequest("ToDoList", "Items", currentThroughput * _scaleFactor );
+                var result = Scaler.AdjustThroughputAsync(scaleRequest).Result;
             }
         }
     }
