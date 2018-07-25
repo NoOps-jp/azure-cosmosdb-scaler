6b43bb536d45b0d50abf0168b312488811ac5a6d
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-07-25 14:36:47 +0900

    Adding Options

--- src/NoOpsJp.CosmosDbScaler/NoOpsJp.CosmosDbScaler/Clients/StreamlinedDocumentClientOptions.cs
+++ src/NoOpsJp.CosmosDbScaler/NoOpsJp.CosmosDbScaler/Clients/StreamlinedDocumentClientOptions.cs
@@ -5,5 +5,10 @@
         public string AccountEndpoint { get; set; }
         public string AccountKey { get; set; }
         public string DatabaseId { get; set; }
+        public string ConnectionMode { get; set; }
+        public string ConnectionProtocol { get; set; }
+        public string ConsistencyLevel { get; set; }
+        public string[] PreferredLocations { get; set; }
+
     }
 }
