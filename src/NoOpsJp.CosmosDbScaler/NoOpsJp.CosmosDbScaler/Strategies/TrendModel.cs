9b044fbc6f0e13531ee627a82ca54502e9b8b406
jroberts <github.robjam@gmail.com>
2018-07-25 17:44:59 +0900

    Add SimpleScaleStrategy

---
+++ src/NoOpsJp.CosmosDbScaler/NoOpsJp.CosmosDbScaler/Strategies/TrendModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NoOpsJp.CosmosDbScaler.Strategies
+{
+    public class TrendModel
+    {
+        public double Intercept { get; set; }
+        public double Slope { get; set; }
+        public int LastRecordIndex { get; set; }
+    }
+}
